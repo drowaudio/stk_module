88770eb317416aa7bb7b9280dfb15c5f2e8592ee
drowland <drowland96@gmail.com>
2012-05-12 20:28:05 +0100

    Updated the licence info.

--- stk.cpp
+++ stk.cpp
@@ -1,24 +1,45 @@
 /*
   ==============================================================================
 
-   This file is part of the JUCE library - "Jules' Utility Class Extensions"
-   Copyright 2004-11 by Raw Material Software Ltd.
+  The Synthesis ToolKit in C++ (STK) is a set of open source audio
+  signal processing and algorithmic synthesis classes written in the
+  C++ programming language. STK was designed to facilitate rapid
+  development of music synthesis and audio processing software, with
+  an emphasis on cross-platform functionality, realtime control,
+  ease of use, and educational example code.  STK currently runs
+  with realtime support (audio and MIDI) on Linux, Macintosh OS X,
+  and Windows computer platforms. Generic, non-realtime support has
+  been tested under NeXTStep, Sun, and other platforms and should
+  work with any standard C++ compiler.
 
-  ------------------------------------------------------------------------------
+  STK WWW site: http://ccrma.stanford.edu/software/stk/
 
-   JUCE can be redistributed and/or modified under the terms of the GNU General
-   Public License (Version 2), as published by the Free Software Foundation.
-   A copy of the license is included in the JUCE distribution, or can be found
-   online at www.gnu.org/licenses.
+  The Synthesis ToolKit in C++ (STK)
+  Copyright (c) 1995-2011 Perry R. Cook and Gary P. Scavone
 
-   JUCE is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-   A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
 
-  ------------------------------------------------------------------------------
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
 
-   To release a closed-source product which uses JUCE, commercial licenses are
-   available: visit www.rawmaterialsoftware.com/juce for more information.
+  Any person wishing to distribute modifications to the Software is
+  asked to send the modifications to the original developer so that
+  they can be incorporated into the canonical version.  This is,
+  however, not a binding provision of this license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+  ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
   ==============================================================================
 */
